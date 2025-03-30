50ed00157078f3d06d0cf19b44efeb8b922a160e
ILikeCodingg5565 <136778324+ILikeCodingg5565@users.noreply.github.com>
2025-03-31 09:19:55 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function downloadClient() {
+    alert("Downloading Cyemer Client...");
+}
+
